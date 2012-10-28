abd54f6fbe33d1759c88509a8a61d688d91556e9
Takahiro Noda <takahiro.noda@gmail.com>
2012-10-28 19:04:16 +0900

    Nitpick the project description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.tnoda/simple-crypto "0.1.0"
-  :description "A Clojure library designed to perform weak string encryption, based on 128-bit AES."
+  :description "A Clojure library designed to perform weak encryption, based on 128-bit AES."
   :url "https://github.com/tnoda/simple-crypto"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
